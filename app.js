9008d6e9b78f432aef63e6bd658ee93ba8d765f9
chaeyoung <chaemaa00@gmail.com>
2023-05-04 22:27:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5; //const는 바꾸지 않는 값 constant  숫자
+const b = 2;
+let myName = "chaeyoung"; //let은 뭔가 새로운걸 생성하고 싶을때 씀. 그 후에 나오는 것은 변수명 앞에 let을 달지 않아도됨
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "jin chaeyoung";
+console.log("your new name is " + myName);
