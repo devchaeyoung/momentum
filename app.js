1e95b9a58c13fc49b26cc54d04cb18f7e0258c39
chaeyoung <chaemaa00@gmail.com>
2023-05-05 17:24:23 +0900

    object에대한 이해

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-//데이터타입을 그룹으로 묶기위한 좋은 방법, 나열하기 위한 방법
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const player = {
+  name: "emma", // It's property
+  points: 10,
+  pretty: true,
+};
 
-//dayOfweek의 1번째 element를 알려주세요!
+console.log(player);
+console.log(player.name);
+//console = the object
+//log = this is anywhere
+//player.name 에 접근하는 또 다른 방법
+console.log(player["name"]);
+//프로퍼티의 객체(object)를 바꾸는 방법
+console.log(player);
+player.points = 50;
+console.log(player);
+//const는 바꿀 수 없지만 그런 작업은 우리가 하는 것이 아님
+//단 오류는 아래처럼 전체의 property값은 하나로 바꾸려할 때 일어남
+// player = false;
+// console.log(player);
 
-// Get Item from Array
-console.log(daysOfWeek[0]);
+player.lastName = "Jin";
+console.log(player);
 
-// Add one more day to the array
-daysOfWeek.push("happyDay");
-
-console.log(daysOfWeek[7]);
+player.points = player.points + 15;
+console.log(player.points);
