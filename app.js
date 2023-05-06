d772d32c0616bb551dd26d52abe4ce5c2aa4f23d
chaeyoung <chaemaa00@gmail.com>
2023-05-06 15:16:46 +0900

    function part1

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-const player = {
-  name: "emma", // It's property
-  points: 10,
-  pretty: true,
-};
+player.point; // '()'가 없는 이유 : 함수가 아니기 때문에
+// function 은 계속 반복해서 사용할 수 있는 코드조각
+//어떤 이름이든 hello를 하는 코드를 짜보자!
 
-console.log(player);
-console.log(player.name);
-//console = the object
-//log = this is anywhere
-//player.name 에 접근하는 또 다른 방법
-console.log(player["name"]);
-//프로퍼티의 객체(object)를 바꾸는 방법
-console.log(player);
-player.points = 50;
-console.log(player);
-//const는 바꿀 수 없지만 그런 작업은 우리가 하는 것이 아님
-//단 오류는 아래처럼 전체의 property값은 하나로 바꾸려할 때 일어남
-// player = false;
-// console.log(player);
+console.log("hello my name is chaeyoung");
+//라는 식으로 이름을 하나하나 입력하면 비효율적! 이럴 경우 function응 사용할 수 있다.
 
-player.lastName = "Jin";
-console.log(player);
+function sayHello() {
+  console.log("Hello!");
+}
+//argument=인수
 
-player.points = player.points + 15;
-console.log(player.points);
+//function (a){
+//  var a
+//console.log("Hello! " + a);
+//  }
