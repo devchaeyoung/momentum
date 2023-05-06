396d9d27a0caf89919999dbd2eaba37cb20c5fbb
chaeyoung <chaemaa00@gmail.com>
2023-05-06 16:18:18 +0900

    func part.2 : How make function

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-player.point; // '()'가 없는 이유 : 함수가 아니기 때문에
-// function 은 계속 반복해서 사용할 수 있는 코드조각
-//어떤 이름이든 hello를 하는 코드를 짜보자!
+console.log("자세한 공부기록이 궁금하다면 github.com/devchaeyoung/momentum/commits의 히스토리를 확인해주세요!");
 
-console.log("hello my name is chaeyoung");
-//라는 식으로 이름을 하나하나 입력하면 비효율적! 이럴 경우 function응 사용할 수 있다.
-
-function sayHello() {
-  console.log("Hello!");
+function sayHello(nameOfPerson, age) {
+  console.log("Hello! My name is " + nameOfPerson + ". and I'm " + age);
 }
-//argument=인수
 
-//function (a){
-//  var a
-//console.log("Hello! " + a);
-//  }
+sayHello("lula", 18);
+sayHello("minah", 23);
+sayHello("jinah", 24);
+sayHello("jinwoo", 21);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+function divide(a, b) {
+  console.log(a / b);
+}
+plus(3 + 50);
+divide(100 / 3);
