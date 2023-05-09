fbfb116fcf6176dad3e3ede04ba48a6ab82f712f
chaeyoung <chaemaa00@gmail.com>
2023-05-09 15:32:02 +0900

    return은 variable에 value값을 줄 수 있지

--- app.js
+++ app.js
@@ -1,62 +1,42 @@
-function plus(a, b) {
-  console.log(a + b);
+//return
+//console.log를 사용하지않고 함수안에서 값을 확인하는 법
+
+const yearOfBirth = 1800;
+function calculateKrAge(theYearOfBirth) {
+  let thisYear = 2023;
+  thisYear - theYearOfBirth + 1;
+  return "Hello";
 }
 
-plus(2, 2);
-//function의 목적은 같은 코드로 여러작업을 반복하기 위함
+// 아래코드를 보고 데이터값이 들어가는 흐름 따라 가보기.
+const krAge = calculateKrAge(yearOfBirth);
 
-alert("lalalalalal");
-
-function minusFive(potato) {
-  console.log(potato - 5);
-}
-minusFive(3, 5, 6, 86, 42, 5, 7, 18);
-// have so many argument But this function is affected only one argument in first.
+console.log(krAge);
 
 const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   multiply: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   powerOf: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
   rest: function (a, b) {
-    console.log(a % b);
+    return a % b;
   },
 };
 
-calculator.add(1, 3);
-calculator.minus(4, 1);
-calculator.multiply(56, 2);
-calculator.divide(2048, 2);
-calculator.powerOf(2, 11);
-calculator.rest(3.15, 2);
-
-//과목 갯수별로 평균을 낼 수 있는 계산기 만들기
-const calculator2 = {
-  two: function (a, b) {
-    console.log((a + b) / 2);
-  },
-  three: function (a, b, c) {
-    console.log((a + b + c) / 3);
-  },
-  four: function (a, b, c, d) {
-    console.log((a + b + c + d) / 4);
-  },
-  five: function (a, b, c, d, e) {
-    const average = (a + b + c + d + e) / 5;
-    console.log(average);
-  },
-};
-
-calculator2.two(5, 1);
-//calculator.과목갯수 를 입력하면 됨
+const plusResult = calculator.add(1, 3);
+const minusResult = calculator.minus(4, 1);
+const multiplyResult = calculator.multiply(56, 2);
+const divideResult = calculator.divide(2048, 2);
+const powerOfResult = calculator.powerOf(2, 11);
+const restOfResult = calculator.rest(3.15, 2);
