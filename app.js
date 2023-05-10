1e8587b036ae4ffb4975c65cec3439784ef01114
chaeyoung <chaemaa00@gmail.com>
2023-05-10 12:52:00 +0900

    we can check age to drink

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-//prompt
-//parsedeIn();
-//NaN : Nat a number
+//프로그래머로 일하다보면 조건이 하나보다 많은 경우가 많다.
+//그럴 때 사용하는 것이 else if!
 
-const age = parseInt(prompt("How old are you?"));
+const age = parseInt(prompt("How old you?"));
 
-if (isNaN(age)) {
-  console.log("please write a number");
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number.");
+} else if (age < 20) {
+  console.log("you are too young.");
+} else if (age >= 20 && age <= 50) {
+  console.log("You can Drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise.");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 } else {
-  console.log("Thank you for writing your age");
+  console.log("You can't Drink.");
 }
-//다음 commit은?!
-//18세이하인지 확인하기(음주불가)
-//50세 초과면 음주를 그만하는게 좋지않을까요. 라고 말하기
+//AND연산자(모든 값이 true여야 boolean타입이 true로 value값을 받을 수 있다.)
+//OR연산자(하나라도 true이면 boolean타입이 true로 value값을 받고 모든값이 false여야 false로 value값을 받을 수 있다.)
