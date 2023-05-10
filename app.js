c0f07a1d5e3a5101dd745006ccd920436560ea8a
chaeyoung <chaemaa00@gmail.com>
2023-05-10 12:28:22 +0900

    conditionals part.2

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 //NaN : Nat a number
 
 const age = parseInt(prompt("How old are you?"));
-console.log(age);
+
+if (isNaN(age)) {
+  console.log("please write a number");
+} else {
+  console.log("Thank you for writing your age");
+}
+//다음 commit은?!
+//18세이하인지 확인하기(음주불가)
+//50세 초과면 음주를 그만하는게 좋지않을까요. 라고 말하기
