cab0e03dcb8f1871497370d8c9465d8533656ec3
chaeyoung <chaemaa00@gmail.com>
2023-05-11 16:08:31 +0900

    Searching for Elements

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 //자바스크립트가 브라우저에서 어떻게 움직이는 가
 document.title = "Hello! from JS";
+//cannot set property 'innerText' of null
+
+const title = document.getElementById("title");
+title.innerText = "Got you!";
+console.log(title.className);
