4ba44170c2afbd058a96d94e14db240b9fbc84c6
chaeyoung <chaemaa00@gmail.com>
2023-06-11 13:58:25 +0900

    230611

--- app.js
+++ app.js
@@ -4,4 +4,18 @@ function handleTitleClick() {
   console.log("title was click!");
 }
 
+function handleMouseEnter() {
+  console.log("mouse is here!");
+  title.innerText = "Mouse is Here!";
+}
+
+function handleMouseLeave() {
+  console.log("mouse leaved");
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+
+title.addEventListener("mouseenter", handleMouseEnter);
+
+title.addEventListener("mouseleave", handleMouseLeave);
