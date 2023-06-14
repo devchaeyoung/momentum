c76deafb51014e1dd3cb939ec179b44001105cb2
chaeyoung <chaemaa00@gmail.com>
2023-06-14 21:15:15 +0900

    230614

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const h1 = document.querySelector(".hello > h1");
+// const h1 = document.querySelector(".hello > h1");
 
-function handleTitleClick() {
-  console.log("title was click!");
+// function handleTitleClick() {
+//   console.log("title was click!");
+// }
+
+// h1.onclick = handleTitleClick;
+// h1.addEventListener("click", handleTitleClick);
+//위 두 코드는 같지만 addEventListener의 경우 removeEventListener를 사용할 수 있으므로 더 선호해서 사용하는 편이다.
+
+function handleWindowOffline() {
+  alert("Please checking your Network :(");
 }
 
-h1.onclick = handleTitleClick;
-h1.addEventListener("click", handleTitleClick);
-//위 두 코드는 같지만 addEventListener의 경우 removeEventListener를 사용할 수 있으므로 더 선호해서 사용하는 편이다.
+function handleWindowOnline() {
+  alert("success! :)");
+}
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+
+const h1 = document.querySelector(".hello > h1");
+
+function handleTextColor() {
+  if (h1.style.color === "blue") {
+    h1.style.color = "tomato";
+  } else {
+    h1.style.color = "blue";
+  }
+}
+
+h1.addEventListener("click", handleTextColor);
