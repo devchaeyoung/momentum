6ae70889c9ec6423135b12f309ea249378e27d86
devchaeyoung <chaemaa00@gmail.com>
2023-07-26 01:26:07 +0900

    preventDefault

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginBtnClick(e) {
+  e.preventDefault(); //어떤 event가 기본행동 (페이지 새로고침)을 하지 않도록 막는 것
+  console.log(e);
 }
 
-loginForm.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginBtnClick);
