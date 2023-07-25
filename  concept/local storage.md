84188b59f56a9d04081ec03a63c033d21aff3cd1
devchaeyoung <chaemaa00@gmail.com>
2023-07-26 02:50:49 +0900

    ch: 자주쓰는 문자열 상수호출 해주기

---  concept/local storage.md
+++  concept/local storage.md
@@ -2,5 +2,5 @@ console.log 를 통해 확인할 수도 있고
 application 을 통해서도 확인할 수 있다.
 [MDN](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
 localStorage.setItem(key, value)
-localStorage.getItem(key)
+localStorage.getItem(key) -> key 값이 없으면 Null!
 localStorage.removeItem(key)
--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+//본인이 string을 반복해서 쓸 경우에는 변수에 할당해주기!
 
 function onLoginBtnClick(e) {
   e.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginBtnClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+//이렇게 매번 변수에 할당하는 이유는 오타가 났을때 바로 찾을 수 있기 위해서!
+
+if (savedUsername === null) {
+  //form 태그를 가져오기 show the form
+} else {
+  //show the greeting
+}
