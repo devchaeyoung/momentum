9a77687fa6cb8b22de3b9e886b471cebbe4f3208
devchaeyoung <chaemaa00@gmail.com>
2023-07-26 02:23:43 +0900

    Rename + ch : template literal

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = documnet.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginBtnClick(e) {
   e.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(e) {
-  e.preventDefault(); //?? 기다렸다 작동되는데? 뭐지
-  console.log(e);
-  alert("clicked!"); // alert자체가 새로고침 되는 걸 막아줌. preventDefault랑 쓸 필요 X
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginBtnClick);
-link.addEventListener("click", handleLinkClick);
