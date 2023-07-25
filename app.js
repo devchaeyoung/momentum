015ca9137ea8c9be4c8dd460b82b33ff0a418e9c
devchaeyoung <chaemaa00@gmail.com>
2023-07-26 00:27:43 +0900

    event

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// const h1 = document.querySelector(".hello > h1");
-
-// function handleTitleClick() {
-//   console.log("title was click!");
-// }
-
-// h1.onclick = handleTitleClick;
-// h1.addEventListener("click", handleTitleClick);
-//위 두 코드는 같지만 addEventListener의 경우 removeEventListener를 사용할 수 있으므로 더 선호해서 사용하는 편이다.
-
 function handleWindowOffline() {
   alert("Please checking your Network :(");
 }
