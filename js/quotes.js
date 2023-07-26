4d5f258a752256cb58dc5dca03806290e3d393af
devchaeyoung <chaemaa00@gmail.com>
2023-07-26 23:29:10 +0900

    ch:quotes

---
+++ js/quotes.js
@@ -0,0 +1,57 @@
+const quotes = [
+  {
+    quote: "The Way to Get Started is to Quit Talking and Begin Doing.",
+    author: "Walt Disney",
+  },
+
+  {
+    quote: "Life is what happens to you while you're busy making other plans.",
+    author: "john Lennon",
+  },
+  {
+    quote:
+      "The world is a book and those who do not travel read only one page.",
+    author: "Saint Augustine",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "Only I can change me Life,no one can do it for me",
+    author: "Carol Burnett",
+  },
+  {
+    quote: "Turn your wounds into wisdom.",
+    author: "Oprah Gail Winfrey",
+  },
+  {
+    quote:
+      "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston S. Churchill",
+  },
+  {
+    quote: "It is kind of fun to do the impossible",
+    author: "Walt Disney",
+  },
+  {
+    quote: "I find that the harder I work, the more luck I seem to have. ",
+    author: "Thomas Jefferson",
+  },
+  {
+    quote: "Try not to become a man of success. Rather become a man of value.",
+    author: "Albert Einstein",
+  },
+];
+
+//https://www.amazingtalker.co.kr/blog/ko/kr-en/60488/
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0 - 9]);
