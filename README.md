e755751679a9843e390a949bec8420a16880d121
devchaeyoung <chaemaa00@gmail.com>
2023-09-24 20:31:19 +0900

    chart1: Delete 버튼 만들기

---
+++ README.md
@@ -0,0 +1,12 @@
+# Momentum
+
+## 학습자료
+
+- [Nomard Coder JS로 Chrome App 만들기](https://nomadcoders.co/javascript-for-beginners)
+
+## 버전 설명
+
+- **0.0.1v** 기존 Nomard Coder 강의 클론앱
+- **23.10.0**
+
+##
--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,31 @@ const toDoInput = toDoForm.querySelector("input");
 // = document.querySelector("#todo-form input")  두번 찾을 필요 없음
 const toDoList = document.querySelector("#todo-list");
 
+/**목록을 삭제하는 함수
+ * 화살표함수로 바로 실행하는게 좋을 지 함수로 빼서 사용해주는 지 좋을지 고민하기
+ * React에서 따로 compoenet로 써도 좋을 거 같기도..?
+ */
+function deleteToDo(e) {
+  // console을 통해 삭제할 target을 찾아서 지울 수 있다.
+  const li = e.target.parentElement;
+  li.remove;
+
+  // console.log(e); // event가 일어난 자리를 path를 통해 확인 가능!
+  // console.dir(e.target); // event가 일어난 target의 parent를 확인 가능!
+}
 function paintToDo(newToDo) {
   console.log(`I will paint ${newToDo}`);
   const li = document.createElement("li");
+
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newToDo;
+
+  const delButton = document.createElement("button");
+  delButton.innerText = "❎";
+  delButton.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(delButton);
   toDoList.appendChild(li);
 }
 
@@ -20,9 +39,7 @@ function checkListToDo(newToDo) {
   console.log(`I will paint ${newToDo}`);
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  const span = document.createElement("span");
-  checkbox.appendChild(span);
-  span.innerText = newToDo;
+  checkbox.innerText = newToDo;
   toDoList.appendChild(checkbox);
 }
 
